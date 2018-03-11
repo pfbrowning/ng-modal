720d128d92b70359c5c9d01d77d9c8517bca215d
Patrick Browning <pfbrowning943@yahoo.com>
2018-03-11 15:48:37 -0400

    Adding missing barrel exports which were causing problems with prod mode

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ModalWindowComponent } from './components/modal-window.component';
 import { ModalManagerService } from './services/modal-manager.service';
 
+export * from './components/modal-window.component';
+export * from './services/modal-manager.service';
+
 @NgModule({
   imports: [
     CommonModule
